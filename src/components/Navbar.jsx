d78e8f35707ea87964099bfdb75243c6b68602c1
Suraj <surajbahadur58@gmail.com>
2023-04-16 07:54:35 +0530

    ui changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,18 @@ const Navbar = () => {
   return (
     <div className="my-2">
       <div className="flex justify-between">
+        <Link  to="/">
         <h1 className="xs:text-xl  font-pacifico md:text-3xl text-[#8A2E38]">
           Shopping Store
         </h1>
+          </Link>
+
+        
 
         <div className="flex justify-center items-center">
-          <Link className="text-lg  px-4 hover:font-semibold" to="/">
+          {/* <Link className="text-lg  px-4 hover:font-semibold" to="/">
             Home
-          </Link>
+          </Link> */}
           <h1 className="text-white"> {" Shopping bag asdasdasdasass "}</h1>
           <Link
             className="bg-[#8A2E38] fixed text-white py-2  justify-center  px-4   md:block"
